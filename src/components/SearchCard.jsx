1ea2633fb57edb1956077c70b9aab623445bb5fd
Joff317 <j.freixanet@hotmail.com>
2023-12-09 11:21:22 +0100

    first part of query

---
+++ src/components/SearchCard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Button, Card, CardFooter, CardHeader, Image } from "@nextui-org/react";
+import { Link } from "react-router-dom";
+
+const SearchCard = (props) => {
+  //   console.log(props);
+  return (
+    <>
+      <div key={props.plant.id} className="col-span-1">
+        <Card
+          isFooterBlurred
+          className="w-[250px] h-[300px] col-span-12 sm:col-span-5"
+        >
+          <CardHeader className="absolute z-10 top-1 flex-col items-start">
+            <h4
+              className=" font-medium text-2xl"
+              style={
+                props.plant.common_name && props.plant.image_url
+                  ? {
+                      textColor: "white",
+                    }
+                  : {
+                      textColor: "black",
+                    }
+              }
+            >
+              {props.plant.common_name
+                ? props.plant.common_name
+                : props.plant.slug}
+            </h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card example background"
+            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover text-black"
+            src={
+              props.plant.image_url
+                ? props.plant.image_url
+                : "image not available"
+            }
+          />
+          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+            <div>
+              <p className="text-black text-tiny">
+                Discover in : {props.plant.year}
+              </p>
+              <p className="text-black text-tiny">
+                Family : {props.plant.family}
+              </p>
+            </div>
+            <Link to={`/${props.plant.id}`}>
+              <Button
+                className="text-tiny"
+                color="primary"
+                radius="full"
+                size="sm"
+              >
+                See Details
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SearchCard;
--- src/pages/SearchPlantPage.jsx
+++ src/pages/SearchPlantPage.jsx
@@ -1,5 +1,62 @@
+import { Card } from "@nextui-org/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import HomeCard from "../components/HomeCard";
+import SearchCard from "../components/SearchCard";
+
 export default function SearchPlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [query, setGetQuery] = useState("coconut");
+
+  const matchPlantNames = (inputValue, plantName) => {
+    if (inputValue.length >= 3) {
+      const regex = new RegExp(inputValue, "i");
+      return regex.test(plantName);
+    }
+    return false;
+  };
+
+  const fetchOneSpecificPlant = () => {
+    if (query.trim() !== "") {
+      axios
+        .get(
+          `https://server-express-eight.vercel.app/plants/search?query=${query}`
+        )
+        .then((res) => {
+          console.log(res.data);
+          setPlants(res.data.data);
+        })
+        .catch((e) => console.error(e));
+    }
+  };
+
+  useEffect(() => {
+    fetchOneSpecificPlant();
+  }, [query]);
+
+  const handleQuery = (e) => {
+    e.preventDefault();
+    setGetQuery(e.target.value);
+  };
+
   return (
-    <div>SearchPlantPage</div>
-  )
+    <div className="min-h-full pt-4 w-full flex flex-col items-center justify-center">
+      <label htmlFor="search-plant">Search Specific Plant in English</label>
+      <input
+        type="text"
+        className="text-black"
+        placeholder="search for special beers"
+        value={query}
+        onChange={handleQuery}
+      />
+      <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+        {plants.length > 0 &&
+          plants.map((plant) =>
+            matchPlantNames(query, plant.common_name || plant.slug) ? (
+              <HomeCard key={plant.id} plant={plant} />
+            ) : null
+          )}
+      </div>
+    </div>
+  );
 }
